1093fe4b46e6bcf93038302e8cb38a8451ee4fa7
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2019-05-29 17:11:14 -0700

    changed id=app to id=photos

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const port = 3000;
+const path = require('path');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.static(path.resolve(__dirname, '../client/public')));
+
+// app.get('/:id', (req, res) => {
+//   if (!req.params.id) {
+//     res.status(400);
+//     res.end();
+//   } else {
+//     res.sendFile('index.html', { root: path.resolve(__dirname, '../client/public') });
+//   }
+// });
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
+
+module.exports.app = app;
