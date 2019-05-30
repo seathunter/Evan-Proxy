544dece1f7dcc0d40c886153529960b15c0a73f6
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2019-05-29 19:28:42 -0700

    placed components and added font

--- server/server.js
+++ server/server.js
@@ -7,14 +7,14 @@ const app = express();
 app.use(cors());
 app.use(express.static(path.resolve(__dirname, '../client/public')));
 
-// app.get('/:id', (req, res) => {
-//   if (!req.params.id) {
-//     res.status(400);
-//     res.end();
-//   } else {
-//     res.sendFile('index.html', { root: path.resolve(__dirname, '../client/public') });
-//   }
-// });
+app.get('/:id', (req, res) => {
+  if (!req.params.id) {
+    res.status(400);
+    res.end();
+  } else {
+    res.sendFile('index.html', { root: path.resolve(__dirname, '../client/public') });
+  }
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
